7ba24dc07a38eec75bf9e2de4a1197b84fc15e88
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2025-06-16 20:05:51 +0300

    add services_locator.dart to use get it init get it in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:auvent_flutter_internship_assessment/core/services/services_locator.dart';
 import 'package:auvent_flutter_internship_assessment/root/app_root.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +7,7 @@ Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  ServicesLocator.init();
   runApp(const TaskApp());
 }
 
