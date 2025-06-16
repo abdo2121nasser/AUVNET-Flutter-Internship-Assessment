eff9ffae9faba687adeb5a0ffd6f851a793528ee
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2025-06-16 18:56:32 +0300

    connect  project with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:auvent_flutter_internship_assessment/root/app_root.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const TaskApp());
 }
 
