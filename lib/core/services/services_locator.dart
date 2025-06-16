e6a398b9350895256a657dda9cc6debec71c05d6
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2025-06-16 20:52:18 +0300

    register the AuthenticationByFirebaseDataSource,AuthenticationRepository, SignInUseCase,SignInBloc

--- lib/core/services/services_locator.dart
+++ lib/core/services/services_locator.dart
@@ -1,3 +1,10 @@
+import 'package:auvent_flutter_internship_assessment/core/utils/usecase/base_usecase.dart';
+import 'package:auvent_flutter_internship_assessment/features/authentication_feature/data_layer/data_source/remote/base_authentication_remote_data_source.dart';
+import 'package:auvent_flutter_internship_assessment/features/authentication_feature/data_layer/data_source/remote/authentication_by_firebase_data_source.dart';
+import 'package:auvent_flutter_internship_assessment/features/authentication_feature/data_layer/repositories/authentication_repository.dart';
+import 'package:auvent_flutter_internship_assessment/features/authentication_feature/domain_layer/repositories/base_authentication_repository.dart';
+import 'package:auvent_flutter_internship_assessment/features/authentication_feature/domain_layer/use_cases/sign_in_use_case.dart';
+import 'package:auvent_flutter_internship_assessment/features/authentication_feature/presentaion_layer/controllers/sign_in_bloc/sign_in_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
@@ -5,5 +12,21 @@ final sl = GetIt.instance;
 class ServicesLocator {
  static void init() {
 
+
+   //blocs
+   sl.registerFactory(() => SignInBloc(signInUseCase: sl()));
+
+   //useCase
+   sl.registerLazySingleton(() => SignInUseCase(baseAuthenticationRepository: sl()));
+
+
+   //repositories
+   sl.registerLazySingleton<BaseAuthenticationRepository>(
+           () => AuthenticationRepository(baseAuthenticationRemoteDataSource: sl()));
+
+
+   //data source
+   sl.registerLazySingleton<BaseAuthenticationRemoteDataSource>(
+           () => AuthenticationByFirebaseDataSource());
   }
 }
