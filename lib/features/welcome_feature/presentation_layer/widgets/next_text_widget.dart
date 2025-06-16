40dba06224a54632db23fbaf1246d63db19972ec
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2025-06-16 15:38:31 +0300

    call the move to next content function in the next_text_widget.dart

--- lib/features/welcome_feature/presentation_layer/widgets/next_text_widget.dart
+++ lib/features/welcome_feature/presentation_layer/widgets/next_text_widget.dart
@@ -1,6 +1,7 @@
 import 'package:auvent_flutter_internship_assessment/core/utils/colors/colors.dart';
 import 'package:auvent_flutter_internship_assessment/core/utils/constants/ui_strings.dart';
 import 'package:auvent_flutter_internship_assessment/core/utils/text_styles/style_manager.dart';
+import 'package:auvent_flutter_internship_assessment/features/welcome_feature/presentation_layer/controllers/welcome_bloc/welcome_bloc.dart';
 import 'package:flutter/material.dart';
 
 class NextTextWidget extends StatelessWidget {
@@ -11,7 +12,9 @@ class NextTextWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: (){},
+        onTap: (){
+          WelcomeBloc.get(context).add(MoveToNextContentEvent());
+        },
         child: Text(UiStrings.kNextWord,style: AppTextStyles.rubikRegular14(color: kGreyColor),));
   }
 }
