765539a29b43b0593e112c23d4956b5a8213e4e3
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2025-06-16 18:27:54 +0300

    add blue color to colors.dart move sign_in_form_widget.dart and sign_in_screen_body_widget.dart to sign in widgets folder

--- lib/core/utils/colors/colors.dart
+++ lib/core/utils/colors/colors.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 const Color kBlackColor = Color(0xff333333);
 const Color kWhiteColor = Color(0xffffffff);
 const Color kPurpleColor = Color(0xff8900FE);
+const Color kBlueColor = Color(0xff1877F2);
 const Color kGreyColor = Color(0xff677294);
 const Color kHintColor = Color(0xff000000);
 const Color kTextFieldBackGroundColor = Color(0xffC4C4C4);
