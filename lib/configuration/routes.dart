8bde4105379f49d99f2302a5924b005778f22ea9
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2025-06-16 17:15:56 +0300

    add nawel image to assets and create variable contain its path

--- lib/configuration/routes.dart
+++ lib/configuration/routes.dart
@@ -3,20 +3,22 @@
 import 'package:auvent_flutter_internship_assessment/features/welcome_feature/presentation_layer/screens/welcome_screen.dart';
 import 'package:go_router/go_router.dart';
 
+import '../features/authentication_feature/presentaion_layer/screens/sign_in_screen.dart';
+
 class AppRoute {
   static const welcomeScreen = '/welcome-screen';
   static const signInScreen = '/sing-in-screen';
 
 
   static final router = GoRouter(
-      initialLocation:welcomeScreen,
+      initialLocation:signInScreen,
       routes: [
         GoRoute(
           path: welcomeScreen,
           builder: (context, state) => WelcomeScreen(),
         ),GoRoute(
           path: signInScreen,
-          builder: (context, state) => WelcomeScreen(),
+          builder: (context, state) => SignInScreen(),
         ),
 
       ]
--- lib/core/utils/constants/images.dart
+++ lib/core/utils/constants/images.dart
@@ -1,4 +1,2 @@
-
-
-
-      const String kWelcomeImage='assets/images/welcome_image.png';
+const String kWelcomeImage = 'assets/images/welcome_image.png';
+const String kNawelImage = 'assets/images/nawel.png';
