8969482542adc4bfb3f6677a86881a75f6ae1ddf
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2025-06-16 14:02:16 +0300

    update style manager file to take colors for text

--- lib/core/utils/text_styles/style_manager.dart
+++ lib/core/utils/text_styles/style_manager.dart
@@ -1,61 +1,68 @@
-// text_styles.dart
 import 'package:flutter/material.dart';
+import '../colors/colors.dart';
 import 'font_manager.dart';
 
 class AppTextStyles {
   // DM Sans Medium, size 14
-  static  TextStyle dmSansMedium14 = TextStyle(
+  static TextStyle dmSansMedium14({Color color = kBlackColor}) => TextStyle(
     fontFamily: FontFamily.dmSans,
     fontWeight: FontWeightManager.medium,
     fontSize: FontSize.s14,
+    color: color,
     overflow: TextOverflow.ellipsis,
   );
 
   // DM Sans Medium, size 12
-  static  TextStyle dmSansMedium12 = TextStyle(
+  static TextStyle dmSansMedium12({Color color = kBlackColor}) => TextStyle(
     fontFamily: FontFamily.dmSans,
     fontWeight: FontWeightManager.medium,
     fontSize: FontSize.s12,
+    color: color,
     overflow: TextOverflow.ellipsis,
   );
 
   // DM Sans Bold, size 14
-  static  TextStyle dmSansBold14 = TextStyle(
+  static TextStyle dmSansBold14({Color color = kBlackColor}) => TextStyle(
     fontFamily: FontFamily.dmSans,
     fontWeight: FontWeightManager.bold,
     fontSize: FontSize.s14,
+    color: color,
     overflow: TextOverflow.ellipsis,
   );
 
   // Rubik Medium, size 28
-  static  TextStyle rubikMedium28 = TextStyle(
+  static TextStyle rubikMedium28({Color color = kBlackColor}) => TextStyle(
     fontFamily: FontFamily.rubik,
     fontWeight: FontWeightManager.medium,
     fontSize: FontSize.s28,
+    color: color,
     overflow: TextOverflow.ellipsis,
   );
 
   // Rubik Regular, size 14
-  static TextStyle rubikRegular14 = TextStyle(
+  static TextStyle rubikRegular14({Color color = kBlackColor}) => TextStyle(
     fontFamily: FontFamily.rubik,
     fontWeight: FontWeightManager.regular,
     fontSize: FontSize.s14,
+    color: color,
     overflow: TextOverflow.ellipsis,
   );
 
   // Rubik Medium, size 18
-  static  TextStyle rubikMedium18 = TextStyle(
+  static TextStyle rubikMedium18({Color color = kBlackColor}) => TextStyle(
     fontFamily: FontFamily.rubik,
     fontWeight: FontWeightManager.medium,
     fontSize: FontSize.s18,
+    color: color,
     overflow: TextOverflow.ellipsis,
   );
 
   // Mulish Light, size 14
-  static  TextStyle mulishLight14 = TextStyle(
+  static TextStyle mulishLight14({Color color = kBlackColor}) => TextStyle(
     fontFamily: FontFamily.mulish,
     fontWeight: FontWeightManager.light,
     fontSize: FontSize.s14,
+    color: color,
     overflow: TextOverflow.ellipsis,
   );
 }
