a1414e9ecd1f681b6b70410b5e384729114b38a7
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2025-06-16 15:04:45 +0300

    add the second and third title and subtitle of welcome screen in ui strings

--- lib/core/utils/constants/ui_strings.dart
+++ lib/core/utils/constants/ui_strings.dart
@@ -3,6 +3,12 @@ class UiStrings{
   static const String kFirstWelcomeTitleString='all-in-one delivery';
   static const String kFirstWelcomeSubTitleString='Order groceries, medicines, and meals delivered straight to your door';
 
+  static const String kSecondWelcomeTitleString='User-to-User Delivery';
+  static const String kSecondWelcomeSubTitleString='Send or receive items from other users quickly and easily';
+
+  static const String kThirdWelcomeTitleString='Sales & Discounts';
+  static const String kThirdWelcomeSubTitleString='Discover exclusive sales and deals every day';
+
   static const String kGetStartWord='Get Started';
   static const String kNextWord='next';
 
--- lib/features/welcome_feature/presentation_layer/widgets/welcome_screen_body_widget.dart
+++ lib/features/welcome_feature/presentation_layer/widgets/welcome_screen_body_widget.dart
@@ -1,10 +1,5 @@
-import 'package:auvent_flutter_internship_assessment/core/utils/colors/colors.dart';
-import 'package:auvent_flutter_internship_assessment/core/utils/component/general_button_widget.dart';
-import 'package:auvent_flutter_internship_assessment/core/utils/constants/ui_strings.dart';
-import 'package:auvent_flutter_internship_assessment/core/utils/text_styles/style_manager.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../core/utils/values/app_size.dart';
 import '../widgets/title_and_sub_title_widget.dart';
 import '../widgets/welcome_image_widget.dart';
 import 'buttons_section_widget.dart';
