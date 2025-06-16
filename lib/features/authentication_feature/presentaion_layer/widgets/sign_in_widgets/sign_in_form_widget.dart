270a44185f315f53f8ed15f7041d43a879b71ac1
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2025-06-16 18:28:34 +0300

    create sign_in_form_widget.dart

---
+++ lib/features/authentication_feature/presentaion_layer/widgets/sign_in_widgets/sign_in_form_widget.dart
@@ -0,0 +1,47 @@
+import 'package:auvent_flutter_internship_assessment/core/services/validator_service.dart';
+import 'package:auvent_flutter_internship_assessment/core/utils/constants/images.dart';
+import 'package:auvent_flutter_internship_assessment/core/utils/constants/ui_strings.dart';
+import 'package:auvent_flutter_internship_assessment/core/utils/values/app_size.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/component/general_text_form_field.dart';
+import '../../../../../core/utils/component/local_image_assets_widget.dart';
+
+class SignInFormWidget extends StatelessWidget {
+  final GlobalKey<FormState> globalKey;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+  const SignInFormWidget({
+    super.key,
+    required this.globalKey,
+    required this.emailController,
+    required this.passwordController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: globalKey,
+      child: Column(
+        children: [
+          GeneralTextFormField(
+              controller: emailController,
+              prefixIcon: CupertinoIcons.envelope,
+              hint: UiStrings.kEmailHint,
+              label: UiStrings.kEmailLabel,
+              validator: ValidatorService.validateEmail),
+          SizedBox(
+            height: k20V,
+          ),
+          GeneralTextFormField(
+              controller: passwordController,
+              prefixIcon: CupertinoIcons.lock,
+              hint: UiStrings.kPasswordHint,
+              label: UiStrings.kPasswordLabel,
+              validator: ValidatorService.validatePassword),
+        ],
+      ),
+    );
+  }
+}
