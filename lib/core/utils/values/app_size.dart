e004390f06b6105195f3ba6db21d9cffeebde385
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2025-06-17 15:16:43 +0300

    change home icons size to fit in custom_bottom_navigation_bar_widget.dart

--- lib/core/utils/values/app_size.dart
+++ lib/core/utils/values/app_size.dart
@@ -8,6 +8,7 @@ final double k14V = 14.h;
 final double k16V = 16.h;
 final double k18V = 18.h;
 final double k20V = 20.h;
+final double k40V = 40.h;
 final double k50V = 50.h;
 final double k60V =60.h;
 
@@ -21,6 +22,7 @@ final double k14H = 14.w;
 final double k16H = 16.w;
 final double k18H = 18.w;
 final double k20H = 20.w;
+final double k40H = 40.w;
 
 final double k5R = 5.r;
 final double k8R = 8.r;
--- lib/features/main_shell_feature/presentation_layer/widgets/custom_bottom_navigation_bar_widget.dart
+++ lib/features/main_shell_feature/presentation_layer/widgets/custom_bottom_navigation_bar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:auvent_flutter_internship_assessment/core/utils/colors/colors.dart';
 import 'package:auvent_flutter_internship_assessment/core/utils/constants/images.dart';
+import 'package:auvent_flutter_internship_assessment/core/utils/values/app_size.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -23,8 +24,8 @@ class CustomBottomNavigationBarWidget extends StatelessWidget {
         BottomNavigationBarItem(
           activeIcon: Image.asset(
             kNawelImage,
-            width: 24,
-            height: 24,
+            width: k40H,
+            height: k40V,
           ),
           icon: const Icon(
             CupertinoIcons.home,
