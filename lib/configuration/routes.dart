7590e6cfd22b25f738cb493bb4ca162474355d0a
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2025-06-18 15:54:00 +0300

    finish splash_screen.dart

--- lib/configuration/routes.dart
+++ lib/configuration/routes.dart
@@ -1,7 +1,6 @@
-
-
 import 'package:auvent_flutter_internship_assessment/features/authentication_feature/data_layer/repositories/authentication_repository.dart';
 import 'package:auvent_flutter_internship_assessment/features/authentication_feature/domain_layer/repositories/base_authentication_repository.dart';
+import 'package:auvent_flutter_internship_assessment/features/splash_feature/presentation_layer/screens/splash_screen.dart';
 import 'package:auvent_flutter_internship_assessment/features/welcome_feature/presentation_layer/screens/welcome_screen.dart';
 import 'package:go_router/go_router.dart';
 
@@ -12,31 +11,32 @@ import '../features/authentication_feature/presentaion_layer/screens/sign_up_scr
 import '../features/main_shell_feature/presentation_layer/screens/main_shell_screen.dart';
 
 class AppRoute {
+  static const splashScreen = '/splash-screen';
   static const welcomeScreen = '/welcome-screen';
   static const signInScreen = '/sing-in-screen';
   static const signUpScreen = '/sing-up-screen';
   static const mainShellScreen = '/main-screen';
 
-
-  static final router = GoRouter(
-      initialLocation:sl<IsSignedInUseCase>()()?
-      mainShellScreen:signInScreen,
-      routes: [
-        GoRoute(
-          path: welcomeScreen,
-          builder: (context, state) => const WelcomeScreen(),
-        ),GoRoute(
-          path: signInScreen,
-          builder: (context, state) => const SignInScreen(),
-        ),GoRoute(
-          path: signUpScreen,
-          builder: (context, state) => const SignUpScreen(),
-        ),GoRoute(
-          path: mainShellScreen,
-          builder: (context, state) => const MainShellScreen(),
-        ),
-
-      ]
-  );
-
+  static final router = GoRouter(initialLocation: splashScreen, routes: [
+    GoRoute(
+      path: splashScreen,
+      builder: (context, state) => const SplashScreen(),
+    ),
+    GoRoute(
+      path: welcomeScreen,
+      builder: (context, state) => const WelcomeScreen(),
+    ),
+    GoRoute(
+      path: signInScreen,
+      builder: (context, state) => const SignInScreen(),
+    ),
+    GoRoute(
+      path: signUpScreen,
+      builder: (context, state) => const SignUpScreen(),
+    ),
+    GoRoute(
+      path: mainShellScreen,
+      builder: (context, state) => const MainShellScreen(),
+    ),
+  ]);
 }
